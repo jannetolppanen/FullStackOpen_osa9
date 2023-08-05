2b503c241337f4f0870383b0a14e94e6036d149e
Janne Tolppanen <janne.ks@gmail.com>
2023-08-05 20:35:25 +0300

    9.3 done

--- firstSteps/exerciseCalculator.ts
+++ firstSteps/exerciseCalculator.ts
@@ -8,6 +8,11 @@ interface Result {
   average: number;
 }
 
+interface ParsedHours {
+  target: number;
+  hours: number[];
+}
+
 const calculateAverage = (args: number[]): number => {
   const length = args.length;
   const sum = args.reduce((sum, number) => {
@@ -19,12 +24,12 @@ const calculateAverage = (args: number[]): number => {
 const rateHours = (target: number, average: number): any => {
   let rating = 0;
   let ratingDescription = '';
-  if (average < target ) {
+  if (average < target) {
     rating = 1;
-  } else if (average > target + 0.5) {
+  } else if (average < target + 0.5) {
     rating = 2;
-  } else { 
-    rating = 3
+  } else {
+    rating = 3;
   }
 
   switch (rating) {
@@ -39,13 +44,35 @@ const rateHours = (target: number, average: number): any => {
       break;
   }
 
-return { rating, ratingDescription }
+  return { rating, ratingDescription };
 };
 
-const calculateExercises = (args: number[]): any => {
+const parseExerciseArguments = (args: string[]): ParsedHours => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+
+  const target = Number(args[2]);
+
+  if (isNaN(target)) {
+    throw new Error('Target value was not a number');
+  }
+
+  // Makes an array of hours and check that they are numbers
+  const hours = args.slice(3).map((hour) => {
+    const parsedHour = Number(hour);
+    if (isNaN(parsedHour)) {
+      throw new Error('Training lenght was not a number');
+    }
+    return parsedHour;
+  });
+  return {
+    target,
+    hours,
+  };
+};
+
+const calculateExercises = (target: number, ...args: number[]): Result => {
   const periodLength = args.length;
   const trainingDays = args.filter((num) => num > 0).length;
-  const target = 2;
   const average = calculateAverage(args);
   const success = average >= target;
   const { rating, ratingDescription } = rateHours(target, average);
@@ -61,5 +88,13 @@ const calculateExercises = (args: number[]): any => {
   };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1]));
-
+try {
+  const { target, hours } = parseExerciseArguments(process.argv);
+  console.log(calculateExercises(target, ...hours));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
