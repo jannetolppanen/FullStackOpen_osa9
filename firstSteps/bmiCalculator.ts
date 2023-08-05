39d8fb32c5e6d64c25b4d426f4dce3269e182ac8
Janne Tolppanen <janne.ks@gmail.com>
2023-08-05 19:34:24 +0300

    9.3 bmi calc done

--- firstSteps/bmiCalculator.ts
+++ firstSteps/bmiCalculator.ts
@@ -1,15 +1,46 @@
+interface bmiValues {
+  height: number;
+  weight: number;
+}
 const calculateBmi = (height: number, weight: number): string => {
   const heightInCentimeters = height / 100;
   const bmi = weight / (heightInCentimeters * heightInCentimeters);
+  let result = ''
 
   if (bmi < 18.5) {
-    return 'Underweight';
+    result = 'Underweight';
   } else if (bmi < 25) {
-    return 'Normal (healthy weight)';
+    result = 'Normal (healthy weight)';
   } else if (bmi < 30) {
-    return 'Overweight';
+    result = 'Overweight';
+  } else {
+    result = 'Obese';
   }
-  return 'Obese';
+  console.log(result)
+  return result
 };
 
-console.log(calculateBmi(180, 74));
+const parseArguments = (args: string[]): bmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+try {
+  const { height, weight } = parseArguments(process.argv)
+  calculateBmi(height, weight)
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
