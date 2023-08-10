5b050f5c979572538f805d49869832eb6ab4867c
Janne Tolppanen <janne.ks@gmail.com>
2023-08-10 09:09:29 +0300

    added cors to flightdiary backend

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -1,9 +1,23 @@
+import axios from "axios";
+
 const App = () => {
+
+  interface Weather {
+    id: number;
+    date: string;
+    weather: 'rainy' | 'sunny' | 'windy';
+    visibility: 'good' | 'poor'
+
+  }
+
+  axios.get('http://localhost:3000/api/diaries').then(response => {
+    console.log(response.data)
+  })
 
 
   return (
     <div>
-      Yeet
+      <h1>flight-diary-frontend</h1>
     </div>
 
   );
