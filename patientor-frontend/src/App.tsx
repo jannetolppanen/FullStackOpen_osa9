8231a6248a6597a7779d562c9045b7dcb21bdadd
Janne Tolppanen <janne.ks@gmail.com>
2023-08-13 23:28:19 +0300

    9.24 done

--- patientor-frontend/src/App.tsx
+++ patientor-frontend/src/App.tsx
@@ -4,14 +4,16 @@ import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { Button, Divider, Container, Typography } from '@mui/material';
 
 import { apiBaseUrl } from "./constants";
-import { Patient } from "./types";
+import { Patient, Diagnosis } from "./types";
 
 import patientService from "./services/patients";
+import diagnoseService from "./services/diagnoses"
 import PatientListPage from "./components/PatientListPage";
 import PatientInfo from "./components/PatientInfo";
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([])
 
   useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
@@ -22,6 +24,16 @@ const App = () => {
     };
     void fetchPatientList();
   }, []);
+
+  useEffect(() => {
+
+    const fetchDiagnoseList = async () => {
+      const diagnoses = await diagnoseService.getAll();
+      setDiagnoses(diagnoses)
+    };
+    void fetchDiagnoseList();
+  }, []);
+
   
   return (
     <div className="App">
@@ -35,7 +47,7 @@ const App = () => {
           </Button>
           <Divider hidden />
           <Routes>
-            <Route path="api/patients/:id" element={<PatientInfo patients={patients}  />}  />
+            <Route path="api/patients/:id" element={<PatientInfo patients={patients} diagnoses={diagnoses}  />}  />
             <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
           </Routes>
         </Container>
--- patientor-frontend/src/components/PatientInfo/index.tsx
+++ patientor-frontend/src/components/PatientInfo/index.tsx
@@ -1,4 +1,4 @@
-import { Patient } from '../../types';
+import { Diagnosis, Patient } from '../../types';
 import { useParams } from 'react-router-dom';
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
@@ -8,52 +8,57 @@ import { Gender } from '../../types';
 
 interface Props {
   patients: Patient[];
+  diagnoses: Diagnosis[];
 }
 
-const PatientInfo = ({ patients }: Props) => {
+const PatientInfo = ({ patients, diagnoses }: Props) => {
   const id = useParams().id;
   const patient = patients.find((p) => p.id === id);
 
-  let genderIcon
+  let genderIcon;
 
   switch (patient?.gender) {
     case Gender.Male:
-      genderIcon = <MaleIcon />
-      break
+      genderIcon = <MaleIcon />;
+      break;
     case Gender.Female:
-      genderIcon = <FemaleIcon />
-      break
+      genderIcon = <FemaleIcon />;
+      break;
     case Gender.Other:
-      genderIcon = <TransgenderIcon />
-      break;  
+      genderIcon = <TransgenderIcon />;
+      break;
     default:
-      genderIcon = <HelpOutlineIcon />
+      genderIcon = <HelpOutlineIcon />;
       break;
   }
 
-
   return (
-  <div>
-    <h2>{patient?.name} {genderIcon}</h2>
-    ssn: {patient?.ssn} <br />
-    occupation: {patient?.occupation}
-
-    <h3>entries</h3>
     <div>
-  {patient?.entries.map((p) => (
-    <div key={p.id}>
-      {p.date} {p.description} <br /> <br />
-
-      {p.diagnosisCodes?.map((d) => (
-        <li key={d}>{d}</li>
-      ))}
-
+      <h2>
+        {patient?.name} {genderIcon}
+      </h2>
+      ssn: {patient?.ssn} <br />
+      occupation: {patient?.occupation}
+      <h3>entries</h3>
+      <div>
+        {patient?.entries.map((p) => (
+          <div key={p.id}>
+            {p.date} {p.description} <br /> <br />
+            {p.diagnosisCodes?.map((d) => {
+              const diagnosis = diagnoses.find(
+                (diagnosis) => diagnosis.code === d
+              );
+              return (
+                <li key={d}>
+                  {d} {diagnosis ? diagnosis.name : 'Unknown Diagnosis'}
+                </li>
+              );
+            })}
+          </div>
+        ))}
+      </div>
     </div>
-  ))}
-</div>
-    
-  
-    </div>
-    )};
+  );
+};
 
 export default PatientInfo;
